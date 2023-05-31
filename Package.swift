e184ce45048fb477e40a4a2ac910672beab665da
Miguel Angel Quinones <miguel@miqu.me>
2023-05-31 15:31:49 +0200

    [TIDY] Use maintained secp256k1 library

--- Package.swift
+++ Package.swift
@@ -38,8 +38,8 @@ let package = Package(
         ),
         .package(
             name: "secp256k1",
-            url: "https://github.com/Boilertalk/secp256k1.swift",
-            from: "0.1.0"
+            url: "https://github.com/GigaBitcoin/secp256k1.swift.git",
+            .upToNextMajor(from: "0.6.0")
         )
     ],
     targets: [
